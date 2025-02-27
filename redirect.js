e0a53f16cc72293cac4aa54db4613bf14eb42005
Neetify0 <navaneetboddepalli@gmail.com>
2025-02-27 16:36:30 -0500

    Fix

--- redirect.js
+++ redirect.js
@@ -3,7 +3,7 @@ function enter() {
     window.location.href = '/';
 }
 
-if (window.location.pathname == "/games/" || window.location.pathname == "/iframe/") {
+if (window.location.pathname == "/games/" || window.location.pathname == "/iframe/" || window.location.pathname == "/youtube-player/" || window.location.pathname == "/cobalt/") {
     if (localStorage.getItem('decrypted8290581') !== 'true') {
         window.location.href = '/lock/';
     } else if (window.location.href !== 'about:blank' && window.top === window.self && window.location.pathname !== "/cloak/") {
